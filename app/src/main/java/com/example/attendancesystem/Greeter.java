0968c3a9d30f32f20fb0c739616dd9e3960d8213
arunikaydav42 <arunikayadav42@gmail.com>
2019-09-12 19:25:21 +0530

    solidity files added

---
+++ app/src/main/java/com/example/attendancesystem/Greeter.java
@@ -0,0 +1,178 @@
+package com.example.attendancesystem;
+
+import io.reactivex.Flowable;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.web3j.abi.EventEncoder;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.TypeReference;
+import org.web3j.abi.datatypes.Event;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.Type;
+import org.web3j.abi.datatypes.Utf8String;
+import org.web3j.crypto.Credentials;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameter;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.tx.Contract;
+import org.web3j.tx.TransactionManager;
+import org.web3j.tx.gas.ContractGasProvider;
+
+/**
+ * <p>Auto generated code.
+ * <p><strong>Do not modify!</strong>
+ * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
+ *
+ * <p>Generated with web3j version 4.2.0.
+ */
+public class Greeter extends Contract {
+    private static final String BINARY = "608060405234801561001057600080fd5b5060405161068d38038061068d8339818101604052602081101561003357600080fd5b810190808051604051939291908464010000000082111561005357600080fd5b90830190602082018581111561006857600080fd5b825164010000000081118282018810171561008257600080fd5b82525081516020918201929091019080838360005b838110156100af578181015183820152602001610097565b50505050905090810190601f1680156100dc5780820380516001836020036101000a031916815260200191505b506040525050600080546001600160a01b0319163317905550805161010890600190602084019061010f565b50506101aa565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061015057805160ff191683800117855561017d565b8280016001018555821561017d579182015b8281111561017d578251825591602001919060010190610162565b5061018992915061018d565b5090565b6101a791905b808211156101895760008155600101610193565b90565b6104d4806101b96000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c806341c0e1b5146100465780634ac0d66e14610050578063cfae3217146100f6575b600080fd5b61004e610173565b005b61004e6004803603602081101561006657600080fd5b81019060208101813564010000000081111561008157600080fd5b82018360208201111561009357600080fd5b803590602001918460018302840111640100000000831117156100b557600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600092019190915250929550610181945050505050565b6100fe610371565b6040805160208082528351818301528351919283929083019185019080838360005b83811015610138578181015183820152602001610120565b50505050905090810190601f1680156101655780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b6000546001600160a01b0316ff5b806040518082805190602001908083835b602083106101b15780518252601f199092019160209182019101610192565b6001836020036101000a03801982511681845116808217855250505050505090500191505060405180910390206001604051808280546001816001161561010002031660029004801561023b5780601f1061021957610100808354040283529182019161023b565b820191906000526020600020905b815481529060010190602001808311610227575b505060408051918290038220818352600180546002600019610100838516150201909116049284018390529094507f047dcd1aa8b77b0b943642129c767533eeacd700c7c1eab092b8ce05d2b2faf59350918691819060208201906060830190869080156102ea5780601f106102bf576101008083540402835291602001916102ea565b820191906000526020600020905b8154815290600101906020018083116102cd57829003601f168201915b5050838103825284518152845160209182019186019080838360005b8381101561031e578181015183820152602001610306565b50505050905090810190601f16801561034b5780820380516001836020036101000a031916815260200191505b5094505050505060405180910390a3805161036d906001906020840190610407565b5050565b60018054604080516020601f600260001961010087891615020190951694909404938401819004810282018101909252828152606093909290918301828280156103fc5780601f106103d1576101008083540402835291602001916103fc565b820191906000526020600020905b8154815290600101906020018083116103df57829003601f168201915b505050505090505b90565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061044857805160ff1916838001178555610475565b82800160010185558215610475579182015b8281111561047557825182559160200191906001019061045a565b50610481929150610485565b5090565b61040491905b80821115610481576000815560010161048b56fea265627a7a7231582041a2392c3481d3b6c974898a9b24f279d2929d428fab18b169240d3bb69bec7c64736f6c634300050b0032";
+
+    public static final String FUNC_KILL = "kill";
+
+    public static final String FUNC_NEWGREETING = "newGreeting";
+
+    public static final String FUNC_GREET = "greet";
+
+    public static final Event MODIFIED_EVENT = new Event("Modified", 
+            Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>(true) {}, new TypeReference<Utf8String>(true) {}, new TypeReference<Utf8String>() {}, new TypeReference<Utf8String>() {}));
+    ;
+
+    @Deprecated
+    protected Greeter(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    protected Greeter(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    @Deprecated
+    protected Greeter(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    protected Greeter(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public RemoteCall<TransactionReceipt> kill() {
+        final Function function = new Function(
+                FUNC_KILL, 
+                Arrays.<Type>asList(), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<TransactionReceipt> newGreeting(String _greeting) {
+        final Function function = new Function(
+                FUNC_NEWGREETING, 
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)), 
+                Collections.<TypeReference<?>>emptyList());
+        return executeRemoteCallTransaction(function);
+    }
+
+    public RemoteCall<String> greet() {
+        final Function function = new Function(FUNC_GREET, 
+                Arrays.<Type>asList(), 
+                Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>() {}));
+        return executeRemoteCallSingleValueReturn(function, String.class);
+    }
+
+    public List<ModifiedEventResponse> getModifiedEvents(TransactionReceipt transactionReceipt) {
+        List<Contract.EventValuesWithLog> valueList = extractEventParametersWithLog(MODIFIED_EVENT, transactionReceipt);
+        ArrayList<ModifiedEventResponse> responses = new ArrayList<ModifiedEventResponse>(valueList.size());
+        for (Contract.EventValuesWithLog eventValues : valueList) {
+            ModifiedEventResponse typedResponse = new ModifiedEventResponse();
+            typedResponse.log = eventValues.getLog();
+            typedResponse.oldGreetingIdx = (byte[]) eventValues.getIndexedValues().get(0).getValue();
+            typedResponse.newGreetingIdx = (byte[]) eventValues.getIndexedValues().get(1).getValue();
+            typedResponse.oldGreeting = (String) eventValues.getNonIndexedValues().get(0).getValue();
+            typedResponse.newGreeting = (String) eventValues.getNonIndexedValues().get(1).getValue();
+            responses.add(typedResponse);
+        }
+        return responses;
+    }
+
+    public Flowable<ModifiedEventResponse> modifiedEventFlowable(EthFilter filter) {
+        return web3j.ethLogFlowable(filter).map(new io.reactivex.functions.Function<Log, ModifiedEventResponse>() {
+            @Override
+            public ModifiedEventResponse apply(Log log) {
+                Contract.EventValuesWithLog eventValues = extractEventParametersWithLog(MODIFIED_EVENT, log);
+                ModifiedEventResponse typedResponse = new ModifiedEventResponse();
+                typedResponse.log = log;
+                typedResponse.oldGreetingIdx = (byte[]) eventValues.getIndexedValues().get(0).getValue();
+                typedResponse.newGreetingIdx = (byte[]) eventValues.getIndexedValues().get(1).getValue();
+                typedResponse.oldGreeting = (String) eventValues.getNonIndexedValues().get(0).getValue();
+                typedResponse.newGreeting = (String) eventValues.getNonIndexedValues().get(1).getValue();
+                return typedResponse;
+            }
+        });
+    }
+
+    public Flowable<ModifiedEventResponse> modifiedEventFlowable(DefaultBlockParameter startBlock, DefaultBlockParameter endBlock) {
+        EthFilter filter = new EthFilter(startBlock, endBlock, getContractAddress());
+        filter.addSingleTopic(EventEncoder.encode(MODIFIED_EVENT));
+        return modifiedEventFlowable(filter);
+    }
+
+    @Deprecated
+    public static Greeter load(String contractAddress, Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Greeter(contractAddress, web3j, credentials, gasPrice, gasLimit);
+    }
+
+    @Deprecated
+    public static Greeter load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
+        return new Greeter(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
+    }
+
+    public static Greeter load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
+        return new Greeter(contractAddress, web3j, credentials, contractGasProvider);
+    }
+
+    public static Greeter load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
+        return new Greeter(contractAddress, web3j, transactionManager, contractGasProvider);
+    }
+
+    public static RemoteCall<Greeter> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider, String _greeting) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)));
+        return deployRemoteCall(Greeter.class, web3j, credentials, contractGasProvider, BINARY, encodedConstructor);
+    }
+
+    public static RemoteCall<Greeter> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider, String _greeting) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)));
+        return deployRemoteCall(Greeter.class, web3j, transactionManager, contractGasProvider, BINARY, encodedConstructor);
+    }
+
+    @Deprecated
+    public static RemoteCall<Greeter> deploy(Web3j web3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit, String _greeting) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)));
+        return deployRemoteCall(Greeter.class, web3j, credentials, gasPrice, gasLimit, BINARY, encodedConstructor);
+    }
+
+    @Deprecated
+    public static RemoteCall<Greeter> deploy(Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit, String _greeting) {
+        String encodedConstructor = FunctionEncoder.encodeConstructor(Arrays.<Type>asList(new org.web3j.abi.datatypes.Utf8String(_greeting)));
+        return deployRemoteCall(Greeter.class, web3j, transactionManager, gasPrice, gasLimit, BINARY, encodedConstructor);
+    }
+
+    public static class ModifiedEventResponse {
+        public Log log;
+
+        public byte[] oldGreetingIdx;
+
+        public byte[] newGreetingIdx;
+
+        public String oldGreeting;
+
+        public String newGreeting;
+    }
+}
--- app/src/main/java/com/example/attendancesystem/MainActivity.java
+++ app/src/main/java/com/example/attendancesystem/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.attendancesystem;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
@@ -15,69 +18,139 @@ import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.Transfer;
+import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.tx.gas.DefaultGasProvider;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
 
 import java.io.File;
-import java.util.concurrent.ExecutionException;
+import java.math.BigDecimal;
+import java.security.Provider;
+import java.security.Security;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final Logger log = LoggerFactory.getLogger(MainActivity.class);
     private Web3j web3;
     private final String password = "medium";
-    private String walletPath;
+    private String walletPath, filename;
     private File walletDir;
+    private File path;
+    private String INFURA_URL = "https://rinkeby.infura.io/v3/";
+    private boolean INCLUDE_RAW_RESPONSES = false;
+    private String TOKEN = "32ab89cf19494f4abf3ab9e9b1e6ef89";
+    private EditText mPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mPassword = (EditText) findViewById(R.id.password);
+        path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        System.out.println("+++++++++++++++++++++++++++++++++++++"+path);
+        if (!path.exists()) {
+            path.mkdir();
+        }
+        setupBouncyCastle();
         setContentView(R.layout.activity_main);
-        walletPath = getFilesDir().getAbsolutePath();
+        walletPath = String.valueOf(path);
+        log.debug("Path for the wallet is : " + walletPath);
         walletDir = new File(walletPath);
     }
 
-    public void connectToEthNetwork(View v){
+    private void setupBouncyCastle() {
+        final Provider provider = Security.getProvider(BouncyCastleProvider.PROVIDER_NAME);
+        if (provider == null) {
+            // Web3j will set up the provider lazily when it's first used.
+            return;
+        }
+        if (provider.getClass().equals(BouncyCastleProvider.class)) {
+            // BC with same package name, shouldn't happen in real life.
+            return;
+        }
+        // Android registers its own BC provider. As it might be outdated and might not include
+        // all needed ciphers, we substitute it with a known BC bundled in the app.
+        // Android's BC has its package rewritten to "com.android.org.bouncycastle" and because
+        // of that it's possible to have another BC implementation loaded in VM.
+        Security.removeProvider(BouncyCastleProvider.PROVIDER_NAME);
+        Security.insertProviderAt(new org.spongycastle.jce.provider.BouncyCastleProvider(), 1);
+    }
+
+
+    public void connectToEthNetwork(View v) {
         toastAsync("Connecting to ethereum network...");
-        web3 = Web3j.build(new HttpService("https://rinkeby.infura.io/v3/32ab89cf19494" +
-                "f4abf3ab9e9b1e6ef89"));
-        try{
+        web3 = Web3j.build(new HttpService(INFURA_URL+TOKEN));
+
+        try {
+//            log.info("Connected to Ethereum client version: "
+//                    + web3.web3ClientVersion().send().getWeb3ClientVersion());
             Web3ClientVersion clientVersion = web3.web3ClientVersion().sendAsync().get();
-            if(!clientVersion.hasError()) {
+            if (!clientVersion.hasError()) {
                 toastAsync("Connected");
             } else {
+                toastAsync("Not connected");
                 toastAsync(clientVersion.getError().getMessage());
             }
         } catch (Exception e) {
-            toastAsync(e.getMessage());
+            toastAsync("Error: " + e.getMessage());
         }
     }
 
-    public void createWallet(View v){
+    public void createWallet(View v) {
         try {
-            WalletUtils.generateLightNewWalletFile(password, walletDir);
+            log.info("Creating wallet.....");
+            filename = WalletUtils.generateLightNewWalletFile(password, walletDir);
             toastAsync("wallet generated");
         } catch (Exception e) {
-            toastAsync(e.getMessage());
+            toastAsync("Error: " + e.getMessage());
         }
     }
 
-    public void getAddress(View v){
+    public void getAddress(View v) {
         try {
-            Credentials credentials = WalletUtils.loadCredentials(password, walletDir);
+            Credentials credentials = WalletUtils.loadCredentials(password, path+"/"+filename);
             toastAsync("Your address is :" + credentials.getAddress());
         } catch (Exception e) {
-            toastAsync(e.getMessage());
+            toastAsync("Error: " + e.getMessage());
         }
     }
 
-//    public void sendTrasaction(View v){
-//        try{
-//            Credentials credentials = WalletUtils.loadCredentials(password, walletDir);
-//            TransactionReceipt transactionReceipt = Transfer.sendFunds(web3, credentials, )
-//        }
-//    }
+    public void sendTransaction(View v) {
+        try {
+            Credentials credentials = WalletUtils.loadCredentials(password, path+"/"+filename);
+            TransactionReceipt transactionReceipt = Transfer.sendFunds(web3,
+                    credentials,
+                    "0x19e03255f667bdfd50a32722df860b1eeaf4d635",
+                    BigDecimal.ONE, Convert.Unit.WEI).send();
+            log.info("Transaction complete, view it at https://rinkeby.etherscan.io/tx/"
+                    + transactionReceipt.getTransactionHash());
+            log.info("Deploying smart contract");
+            ContractGasProvider contractGasProvider = new DefaultGasProvider();
+            Greeter contract = Greeter.deploy(web3, credentials, contractGasProvider, "test").send();
 
-    public void toastAsync(final String message){
+            String contractAddress = contract.getContractAddress();
+            log.info("Smart contract deployed to address " + contractAddress);
+            log.info("Value stored in remote smart contract:  " + contract.greet());
+
+            TransactionReceipt transactionReceipt1 = contract.newGreeting("Well hello agai").send();
+            log.info("New value stored in remote smart contract: " + contract.greet().send());
+
+            for (Greeter.ModifiedEventResponse event : contract.getModifiedEvents(transactionReceipt1)) {
+                log.info("Modify event fired, previous value: " + event.oldGreeting
+                        + ", new value: " + event.newGreeting);
+                log.info("Indexed event previous value: " + Numeric.toHexString(event.oldGreetingIdx)
+                        + ", new value: " + Numeric.toHexString(event.newGreetingIdx));
+            }
+        } catch (Exception e) {
+            toastAsync("Error: " + e.getMessage());
+        }
+    }
+
+    public void toastAsync(final String message) {
         runOnUiThread(() -> {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
         });
     }
-
 }
+
+
